5d452f3cc490f1e0332abed7a1bdb403df6567b7
Toropov Dmitry <dimonsladkoed@mail.ru>
2020-03-01 20:41:13 +0300

    Task1

--- TemplatTask.cpp
+++ TemplatTask.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
+#include "Stack.h"
 
 int main()
 {
+	Stack<int> stack1 = Stack<int>(3);
+	stack1.push(1);
+	stack1.push(2);
+	stack1.push(3);	
+	Stack<int> stack2 = Stack<int>(2);
+	stack2.push(4);
+	stack2.push(5);
+	Stack<int> stack = StackMerge(stack1, stack2);
+	for (int i = 0; i < 5; i++)
+		std::cout << stack.pop() << std::endl;	
+	/*Stack<int> stack1 = Stack<int>(2);
+	Stack<int> stack2 = Stack<int>(2);
+	stack1.push(1);
+	stack1.push(2);
+	stack2.push(3);
+	Stack<int> mergeStack = StackMerge(stack1, stack2);
+	using namespace std;
+	cout << mergeStack.pop() << endl;
+	cout << mergeStack.pop() << endl;
+	cout << mergeStack.pop() << endl;
+	cout << mergeStack.pop() << endl;*/
     return 0;
 }
 
