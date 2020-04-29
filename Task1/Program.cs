0d470c68b6746f370967661403b6ece3b701ed7f
Toropov Dmitry <dimonsladkoed@mail.ru>
2020-04-29 20:52:00 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,6 +10,21 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Stack<int> stack1 = new Stack<int>(5);
+            Stack<int> stack2 = new Stack<int>(3);
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Push(5);            
+            stack2.Push(6);
+            stack2.Push(7);
+            stack2.Push(8);            
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());           
+            Stack<int> st = Stack<int>.StackMerge(stack1, stack2);          
+            for (int i = st.head; i < st.size; i++)
+                Console.WriteLine(st.Pop());
         }
     }
 }
---
+++ Task1/Stack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Stack<T> : ICloneable
+    {        
+        T[] array;
+        public int size;
+        public int head;
+        bool Empty()
+        {
+            if (head == size)
+                return true;
+            else
+                return false;
+        }
+        bool Full()
+        {
+            if (head == 0)
+                return true;
+            else
+                return false;
+        }
+        public Stack(int size)
+        {
+            this.size = size;
+            this.array = new T[this.size];
+            this.head = size;
+        }
+        public void Push(T element)
+        {
+            if (this.Full())
+                throw new InvalidOperationException("Stack is full!");
+            this.head = this.head - 1;
+            this.array[this.head] = element;
+        }
+        public T Pop()
+        {
+            if (this.Empty())
+                throw new InvalidOperationException("Stack is empty!");
+            T element = this.array[this.head];            
+            this.head = this.head + 1;
+            return element;
+        }        
+        public static Stack<T> StackMerge(Stack<T> first, Stack<T> second)
+        {
+            int size = first.size + second.size;
+            Stack<T> stack = new Stack<T>(size);
+            int index = first.size;
+            while (index != first.head)
+            {
+                index = index - 1;
+                stack.Push(first.array[index]);
+            }
+            index = second.size;
+            while (index != second.head)
+            {
+                index = index - 1;
+                stack.Push(second.array[index]);
+            }
+            return stack;
+        }
+        public object Clone()
+        {
+            Stack<T> stack = new Stack<T>(this.array.Length);
+            stack.array = (T[])this.array.Clone();
+            stack.head = this.head;
+            stack.size = this.size;
+            return stack;
+        }
+    }
+}
